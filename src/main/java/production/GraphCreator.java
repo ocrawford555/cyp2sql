b472b75169d9aad79d8bf14ff1bef5e8878a4392
Oliver Crawford <ojc37@cam.ac.uk>
2017-01-24 14:39:53 +0000

    Refactored artificial graph creator code to be more accurate and cleaner.

--- src/main/java/production/GraphCreator.java
+++ src/main/java/production/GraphCreator.java
@@ -6,67 +6,150 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * Creates an artificial graph for testing purposes.
+ * <p>
+ * The value numVertices and String type in this class should be set before running the main method.
+ * - numVertices is the number of nodes to be included in this graph.
+ * - type refers to the type of graph to be constructed, in terms of its density.
+ * The class can generate three types of graphs:
+ * - sparse
+ * - regular
+ * - dense
+ * These are based on values recovered from a paper online looking at graph density of real world
+ * graphs.
+ * <p>
+ * The graph constructed is based on a domain of webpages and programmers. Webpages are linked to
+ * one another randomly. Programmers can code for zero or more webpages, and these webpages are
+ * owned by another set of people known as owners. These owners also employ programmers, although
+ * they may employ programmers to code for websites which the owners do not own (in this case the
+ * programmer might be a contractor for example). The final relationship in the graph is that
+ * programmers and owners can be friends with one another. Note: a person may be both a programmer
+ * and an owner.
+ * <p>
+ * The array allocations assigns a proportion of the nodes to each label.
+ * <p>
+ * The edges are firstly added to the graph randomly. After the number of edges is greater than the
+ * number of nodes, the edges are added proportionally to the degree of each node. Thus, if one node
+ * has a larger number of edges already attached to it, it is more likely to have another edge attach
+ * to it. These creates slightly more realistic graphs, and helps demonstrate in some way the small
+ * world property that a lot of real world graphs exhibit.
+ * <p>
+ * Features of the graph:
+ * - no loops allowed
+ * - type of relationship of the edge is based on the labels of the nodes. In most cases this is
+ * deterministic (i.e. if the label of the node where the edge is coming from is 'programmer', and
+ * the label of the other node is 'website', then this is a "CODES-FOR" relation.)
+ * - if no type of relationship can be found for two nodes, then algorithm flips the edge and attempts
+ * again. This should add all edges to the graph.
+ * <p>
+ * The output of the module are CSV files for both the labels and the relationships. These can then be
+ * filled with additional metadata, and added to Neo4J through the use of the shell and some existing
+ * queries which can parse the .csv.
+ * <p>
+ * load csv with headers from "file:///csv/owner.csv" AS line
+ * CREATE (a:Owner {personID:toInt(line.ownerID), surname:line.surname,
+ * city:line.city, state: line.state, cars:toInt(line.cars)});
+ * <p>
+ * load csv with headers from "file:///csv/progOwner.csv" AS line
+ * CREATE (a:Owner:Programmer {personID:toInt(line.poID), surname:line.surname,
+ * city:line.city, state: line.state, cars:toInt(line.cars), pets:toInt(line.pets)});
+ * <p>
+ * load csv with headers from "file:///csv/programmer.csv" AS line
+ * CREATE (a:Programmer {personID:toInt(line.progID), surname:line.surname,
+ * city:line.city, state:line.state, pets:toInt(line.pets)});
+ * <p>
+ * load csv with headers from "file:///csv/website.csv" AS line
+ * CREATE (a:Website {webID:toInt(line.webID), host:line.host, domain:line.domain});
+ * <p>
+ * load csv with headers from "file:///csv/CODES_FOR.csv" AS line
+ * MATCH (a {personID:toInt(line.sourceID)}), (b:Website {webID:toInt(line.destID)})
+ * CREATE (a)-[r:CODES_FOR {commits:toInt(line.commits)}]->(b);
+ * <p>
+ * load csv with headers from "file:///csv/EMPLOYS.csv" AS line
+ * MATCH (a {personID:toInt(line.sourceID)}), (b {personID:toInt(line.destID)})
+ * CREATE (a)-[r:EMPLOYS {salary:toInt(line.salary), carShare:line.carShare}]->(b);
+ * <p>
+ * load csv with headers from "file:///csv/FRIENDS.csv" AS line
+ * MATCH (a {personID:toInt(line.sourceID)}), (b {personID:toInt(line.destID)}) CREATE (a)-[r:FRIENDS]->(b);
+ * <p>
+ * load csv with headers from "file:///csv/LINKED_TO.csv" AS line
+ * MATCH (a:Website {webID:toInt(line.sourceID)}), (b:Website {webID:toInt(line.destID)})
+ * CREATE (a)-[r:LINKED_TO {popularity:toInt(line.popularity)}]->(b);
+ * <p>
+ * load csv with headers from "file:///csv/OWNS.csv" AS line
+ * MATCH (a {personID:toInt(line.sourceID)}), (b:Website {webID:toInt(line.destID)})
+ * CREATE (a)-[r:OWNS {salary:toInt(line.salary)}]->(b);
+ */
 public class GraphCreator {
+    // set these before running the main method.
     private static final int numVertices = 500;
+    private static final String type = "SPARSE";
+
+    // density calculation based on d = m / n.
+    // where m is the number of edges, and n is the number of nodes.
     private static final double SPARSE_DEGREE = 7.61;
     private static final double REGULAR_DEGREE = 13.49;
     private static final double DENSE_DEGREE = 25.67;
-
     private static final Random r = new Random();
-
-    private static int numEdges = 0;
-    private static long edgesToAdd;
-    private static int[] degreeOfVertices;
-    private static String[] labelsOfNodes;
-    private static int[][] adjMat;
+    // labels of the nodes in the graph.
     private static String[] labels = {"website", "programmer", "owner", "progOwner"};
-
+    // allocations of the number of nodes to the labels above.
     private static int[] allocations = {
             (int) ((0.7) * numVertices),
             (int) ((0.22) * numVertices),
             (int) ((0.05) * numVertices),
             (int) ((0.03) * numVertices)
     };
+    private static int numEdges = 0;
+    private static long edgesToAdd;
+    private static int[] degreeOfVertices;
+    private static String[] labelsOfNodes;
+
+    private static int[][] adjMat;
 
     public static void main(String args[]) {
-        String type = "SPARSE";
         try {
-            setupAndInitlise(type);
+            // initialise the parameters and arrays.
+            setupAndInitlise();
         } catch (Exception e) {
             System.exit(1);
         }
 
+        // add the edges to the graph.
         addEdges();
 
+        // print basic information about the created graph.
         printInformation();
 
         // write the output of the graph to a local CSV file for importing into Neo4J.
         try {
-            produceCSV(type);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        try {
-            produceCSVLabels(type);
+            produceCSV();
+            produceCSVLabels();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
 
-    private static void produceCSVLabels(String type) throws FileNotFoundException {
+    /**
+     * Produce CSV files for the labels - the additional metadata for each label is
+     * added manually from other CSV files.
+     *
+     * @throws FileNotFoundException - File was not found in location specified.
+     */
+    private static void produceCSVLabels() throws FileNotFoundException {
         StringBuilder web = new StringBuilder();
         StringBuilder prog = new StringBuilder();
         StringBuilder owner = new StringBuilder();
         StringBuilder po = new StringBuilder();
 
-        web.append("webID\n");
-        prog.append("progID\n");
-        owner.append("ownerID\n");
-        po.append("poID\n");
+        web.append("webID,host,domain\n");
+        prog.append("progID,surname,city,state,pets\n");
+        owner.append("ownerID,surname,city,state,cars\n");
+        po.append("poID,surname,city,state,pets,cars\n");
 
+        // add the ids of the nodes for each label type to the correct .csv file.
         for (int i = 0; i < labelsOfNodes.length; i++) {
             switch (labelsOfNodes[i]) {
                 case "website":
@@ -84,23 +167,24 @@ public class GraphCreator {
             }
         }
 
-        PrintWriter pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/website-" + type + ".csv"));
+        PrintWriter pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/website.csv"));
         pw.write(web.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/programmer-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/programmer.csv"));
         pw.write(prog.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/owner-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/owner.csv"));
         pw.write(owner.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/progOwner-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/progOwner.csv"));
         pw.write(po.toString());
         pw.close();
-
     }
 
+    /**
+     * Print information of the graph created.
+     */
     private static void printInformation() {
-        // print out the node degree distribution and density
         System.out.println(Arrays.toString(degreeOfVertices));
         System.out.println("Graph density : " + (double) (numEdges) / (numVertices));
         Arrays.parallelSort(degreeOfVertices);
@@ -108,6 +192,9 @@ public class GraphCreator {
         System.out.println("Maximal degree of node : " + degreeOfVertices[numVertices - 1]);
     }
 
+    /**
+     * Adds edges to the graph.
+     */
     private static void addEdges() {
         // add edges to the graph depending on how sparse/dense the test graph should be
         // the edges are added proportionally to the degree of each node
@@ -150,9 +237,17 @@ public class GraphCreator {
         }
     }
 
-    private static void setupAndInitlise(String sparse) throws Exception {
+    /**
+     * Setup and initialise the graph. This includes:
+     * - determining the number of edges needed to produce the correct density value.
+     * - assigning labels to nodes
+     * - setting all values in the adjacency matrix to 0 (meaning no edge)
+     *
+     * @throws Exception - parameter 'type' was not one of either: "SPARSE", "REGULAR", or "DENSE".
+     */
+    private static void setupAndInitlise() throws Exception {
         // setup the number of edges to add to the artificial graph.
-        switch (sparse) {
+        switch (type) {
             case "SPARSE":
                 edgesToAdd = (long) (numVertices * SPARSE_DEGREE);
                 break;
@@ -201,7 +296,12 @@ public class GraphCreator {
         }
     }
 
-    private static void produceCSV(String type) throws FileNotFoundException {
+    /**
+     * Create CSV files for the relationships.
+     *
+     * @throws FileNotFoundException - file not found in specified location.
+     */
+    private static void produceCSV() throws FileNotFoundException {
         StringBuilder sb_LINKED_TO = new StringBuilder();
         StringBuilder sb_CODES_FOR = new StringBuilder();
         StringBuilder sb_OWNS = new StringBuilder();
@@ -225,19 +325,19 @@ public class GraphCreator {
                         if (typeRel != null) {
                             switch (typeRel) {
                                 case "LINKED_TO":
-                                    sb_LINKED_TO = fillCSV(sb_LINKED_TO, from, to, typeRel, r);
+                                    sb_LINKED_TO = fillCSV(sb_LINKED_TO, from, to, typeRel);
                                     break;
                                 case "CODES_FOR":
-                                    sb_CODES_FOR = fillCSV(sb_CODES_FOR, from, to, typeRel, r);
+                                    sb_CODES_FOR = fillCSV(sb_CODES_FOR, from, to, typeRel);
                                     break;
                                 case "OWNS":
-                                    sb_OWNS = fillCSV(sb_OWNS, from, to, typeRel, r);
+                                    sb_OWNS = fillCSV(sb_OWNS, from, to, typeRel);
                                     break;
                                 case "EMPLOYS":
-                                    sb_EMPLOYS = fillCSV(sb_EMPLOYS, from, to, typeRel, r);
+                                    sb_EMPLOYS = fillCSV(sb_EMPLOYS, from, to, typeRel);
                                     break;
                                 case "FRIENDS":
-                                    sb_FRIENDS = fillCSV(sb_FRIENDS, from, to, typeRel, r);
+                                    sb_FRIENDS = fillCSV(sb_FRIENDS, from, to, typeRel);
                                     break;
                             }
 
@@ -260,26 +360,35 @@ public class GraphCreator {
         sb_FRIENDS = addRemainingEdges(sb_FRIENDS, "FRIENDS", 0);
         sb_FRIENDS = addRemainingEdges(sb_FRIENDS, "FRIENDS", 1);
 
-        PrintWriter pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/LINKED_TO-" + type + ".csv"));
+        PrintWriter pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/LINKED_TO.csv"));
         pw.write(sb_LINKED_TO.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/CODES_FOR-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/CODES_FOR.csv"));
         pw.write(sb_CODES_FOR.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/OWNS-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/OWNS.csv"));
         pw.write(sb_OWNS.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/EMPLOYS-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/EMPLOYS.csv"));
         pw.write(sb_EMPLOYS.toString());
         pw.close();
-        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/FRIENDS-" + type + ".csv"));
+        pw = new PrintWriter(new File("C:/Users/ocraw/Desktop/FRIENDS.csv"));
         pw.write(sb_FRIENDS.toString());
         pw.close();
 
-        System.out.println("done!");
+        System.out.println("Done!");
     }
 
-    private static StringBuilder fillCSV(StringBuilder sb, int from, int to, String typeRel, Random r) {
+    /**
+     * Fill the CSV with the correct data.
+     *
+     * @param sb      - comma separated string to write for each row.
+     * @param from    - index of node where relationship is directed from.
+     * @param to      - index of node where relationship is directed to.
+     * @param typeRel - the type of relationship.
+     * @return - comma separated string with new information in.
+     */
+    private static StringBuilder fillCSV(StringBuilder sb, int from, int to, String typeRel) {
         sb.append(from).append(",").append(to);
 
         switch (typeRel) {
@@ -304,6 +413,14 @@ public class GraphCreator {
         return sb;
     }
 
+    /**
+     * Add remaining edges to the graph.
+     *
+     * @param sb  - comma separated string to write for each row.
+     * @param rel - the type of relationship to add.
+     * @param i   - direction of the relationship - if 1 then inverse from and to indexes.
+     * @return comma separated string with new information to write.
+     */
     private static StringBuilder addRemainingEdges(StringBuilder sb, String rel, int i) {
         for (int from = 0; from < adjMat.length; from++) {
             for (int to = 0; to < adjMat.length; to++) {
@@ -317,7 +434,7 @@ public class GraphCreator {
                     String labelTo = labelsOfNodes[to];
                     String typeRel = workoutrel(labelFrom, labelTo);
                     if (typeRel != null && typeRel.equals(rel)) {
-                        sb = fillCSV(sb, from, to, typeRel, r);
+                        sb = fillCSV(sb, from, to, typeRel);
                         adjMat[from][to] = 0;
                         adjMat[to][from] = 0;
                     }
@@ -327,6 +444,13 @@ public class GraphCreator {
         return sb;
     }
 
+    /**
+     * Workout the relationship between the two nodes, based on their labels only.
+     *
+     * @param labelFrom - label of node where relationship is coming from.
+     * @param labelTo   - label of node where relationship is going to.
+     * @return String value of the type of relationship.
+     */
     private static String workoutrel(String labelFrom, String labelTo) {
         if (labelFrom.equals("website") && labelTo.equals("website")) {
             return "LINKED_TO";
@@ -351,6 +475,12 @@ public class GraphCreator {
         }
     }
 
+    /**
+     * Helped method to output the values in the adjacency matrix.
+     * Note : only really for debugging and small matrices.
+     *
+     * @param adjMat Adjacency matrix to print out.
+     */
     private static void printMatirx(int[][] adjMat) {
         int k = 0;
         System.out.print("          ");
@@ -369,6 +499,13 @@ public class GraphCreator {
         }
     }
 
+    /**
+     * Add edge to the adjacency matrix. No loops are allowed in the graph.
+     *
+     * @param from - index of node where edge coming from.
+     * @param to   - index of the node where edge is going to.
+     * @return True: edge added successfully. False: if edge already exists, or values 'from' and 'to' are equal.
+     */
     private static boolean addEdge(int from, int to) {
         from--;
         to--;
